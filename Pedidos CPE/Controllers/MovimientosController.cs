cbf0ab4dfee9b600d7500b3c6f70e32d1d717e42
pointasty <kbecerra774@gmail.com>
2024-10-17 21:20:59 -0600

    movimientos post implemented

---
+++ Pedidos CPE/Controllers/MovimientosController.cs
@@ -0,0 +1,30 @@
+﻿using Application.DTOs;
+using Application.UseCases.SDK.Movimientos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pedidos_CPE.Controllers
+{
+    public class MovimientosController : Controller
+    {
+        private readonly AddMovimientoSDKUseCase _addMovimientoSDKUseCase;
+        public MovimientosController(AddMovimientoSDKUseCase addMovimientoSDKUseCase)
+        {
+            _addMovimientoSDKUseCase = addMovimientoSDKUseCase;
+        }
+
+        [HttpPost]
+        [Route("Movimientos")]
+        public async Task<ActionResult<ApiResponse>> PostMovimiento(MovimientoDTO movimiento)
+        {
+            try
+            {
+                var movimientoDTO = await _addMovimientoSDKUseCase.Execute(movimiento);
+                return CreatedAtAction("PostMovimiento", new ApiResponse { Message = "Movimiento agregado con éxito", Data = movimientoDTO, Success = true });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
